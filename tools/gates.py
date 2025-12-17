ca72ea704bcde100aab79c8dcee840d7a5a2b658
Corentin Bertrand <corentin.bertrand@eviden.com>
2025-12-17 08:55:24 +0100

    Add some gates

--- tools/gates.py
+++ tools/gates.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 GATES_1QB = {
+    'M': None, # measurement
     'X': np.array([[0, 1], [1, 0]]),
     'Y': np.array([[0, -1j], [1j, 0]]),
     'Z': np.array([[1, 0], [0, -1]]),
     'H': np.array([[1, -1], [1, 1]]) / np.sqrt(2.),
+    'S': np.array([[1., 0.], [0., 1j]]),
+    'T': np.array([[1., 0.], [0., np.exp(1j * np.pi / 4.)]]),
 }
 
 GATES_2QB = {
